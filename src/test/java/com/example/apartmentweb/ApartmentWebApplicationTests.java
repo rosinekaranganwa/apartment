98a9b08cb3526d32ca5599d714b1a2c24a748087
rosinekaranganwa <rosinekaranganwa@gmail.com>
2024-04-25 20:39:37 -0400

    created project

---
+++ src/test/java/com/example/apartmentweb/ApartmentWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apartmentweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
